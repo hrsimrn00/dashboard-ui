30e543a9b069bcdec2f72ba91923b5dbdcd372aa
hrsimrn00 <55506470+hrsimrn00@users.noreply.github.com>
2021-11-09 15:48:38 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import AppStats from './components/AppStats'
 
 function App() {
 
-    const endpoints = ["frontend_order", "printer_order"]
+    const endpoints = ["FrontendOrder", "PrinterOrder"]
 
     const rendered_endpoints = endpoints.map((endpoint) => {
         return <EndpointAudit key={endpoint} endpoint={endpoint}/>
